0ac6925d153bb62f91f2748151900756ba1e60e7
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-02-11 00:34:32 +0100

    Feature/profit calculator (#1)
    * Add CLI
    * Read input
    * Handle errors

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 A Hyperskill project to create a simple cinema room manager. The program will help manage the seats in the cinema room.
 It will display the seats, mark the seats that are already booked, and help a user buy tickets. The program will also
 display statistics about the number of seats and the income from the tickets.
+
+## Test suite
+
+```go
+package main
+
+import "testing"
+
+func TestCalculateProfitForSmallRoom(t *testing.T) {
+	c := NewCinema(4, 5)
+	if c.CalculateProfit() != 200 {
+		t.Error("Expected 200 but got", c.CalculateProfit())
+	}
+}
+
+func TestCalculateProfitForBigRoom(t *testing.T) {
+	c := main.NewCinema(8, 9)
+	if c.CalculateProfit() != 648 {
+		t.Error("Expected 648 but got", c.CalculateProfit())
+	}
+}
+
+func TestCalculateProfitForOddNumberOfRows(t *testing.T) {
+	c := main.NewCinema(9, 7)
+	if c.CalculateProfit() != 560 {
+		t.Error("Expected 560 but got", c.CalculateProfit())
+	}
+}
+```
--- main.go
+++ main.go
@@ -1,16 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+	"os"
+)
+
+const priceSmallRoom = 10
+const priceBigRoom = 8
 
 func main() {
-	cinema := `Cinema:
-  1 2 3 4 5 6 7 8
-1 S S S S S S S S
-2 S S S S S S S S
-3 S S S S S S S S
-4 S S S S S S S S
-5 S S S S S S S S
-6 S S S S S S S S
-7 S S S S S S S S`
-	fmt.Println(cinema)
+
+	rows := getRows()
+	seats := getSeats()
+
+	c := NewCinema(rows, seats)
+	profit := c.CalculateProfit()
+	fmt.Printf("Total income:\n$%d", profit)
+
+}
+
+type Cinema struct {
+	Rows  int
+	Seats int
+}
+
+func NewCinema(rows, seats int) *Cinema {
+	return &Cinema{rows, seats}
+}
+
+func (c *Cinema) CalculateProfit() int {
+	s := c.Rows * c.Seats
+	if s < 60 {
+		return s * priceSmallRoom
+	} else {
+		frontRowsProfit := math.Floor(float64(c.Rows)/2) * float64(c.Seats*priceSmallRoom)
+		backRowsProfit := math.Ceil(float64(c.Rows)/2) * float64(c.Seats*priceBigRoom)
+		return int(frontRowsProfit + backRowsProfit)
+	}
+
+}
+func getSeats() int {
+	fmt.Println("Enter the number of seats in each row:")
+	seats, err := getIntFromUser()
+	checkError(err)
+	return seats
+}
+
+func getRows() int {
+	fmt.Println("Enter the number of rows:")
+	rows, err := getIntFromUser()
+	checkError(err)
+	return rows
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func getIntFromUser() (int, error) {
+	var input int
+	_, err := fmt.Scan(&input)
+	if err != nil {
+		return -1, errors.New("error reading from user")
+	}
+	return input, nil
 }
