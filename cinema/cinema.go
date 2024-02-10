1d8bfde105a0bb7dd414c9d58630aefe611f9396
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2024-02-10 23:36:33 +0100

    Implement profit calculator

--- cinema/cinema.go
+++ cinema/cinema.go
@@ -1,1 +1,23 @@
 package cinema
+
+type Cinema struct {
+	Rows  int
+	Seats int
+}
+
+func NewCinema(rows, seats int) *Cinema {
+	return &Cinema{rows, seats}
+}
+
+const priceSmallRoom = 10
+const priceBigRoom = 8
+
+func (c *Cinema) CalculateProfit() int {
+	seats := c.Rows * c.Seats
+	if seats < 60 {
+		return seats * priceSmallRoom
+	} else {
+		return seats / 2 * (priceBigRoom + priceSmallRoom)
+	}
+
+}
--- cinema/cinema_test.go
+++ cinema/cinema_test.go
@@ -1,1 +1,17 @@
 package cinema
+
+import "testing"
+
+func TestCalculateProfitForSmallRoom(t *testing.T) {
+	c := NewCinema(4, 5)
+	if c.CalculateProfit() != 200 {
+		t.Error("Expected 200 but got", c.CalculateProfit())
+	}
+}
+
+func TestCalculateProfitForBigRoom(t *testing.T) {
+	c := NewCinema(8, 9)
+	if c.CalculateProfit() != 648 {
+		t.Error("Expected 648 but got", c.CalculateProfit())
+	}
+}
